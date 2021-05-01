465eda7761e6a5dda7d9f0fd4fccb745769fb767
DESKTOP-0L66HQK\ADMINS <vodoanhoanglong10a9@gmail.com>
2021-05-01 14:18:39 +0700

    ADD

--- BankManager/src/GUI/MainFrame.java
+++ BankManager/src/GUI/MainFrame.java
@@ -1,7 +1,6 @@
 package GUI;
 
 import javax.swing.*;
-import javax.swing.plaf.DimensionUIResource;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -9,7 +8,6 @@ import java.awt.event.MouseEvent;
 public class MainFrame extends JFrame
 {
     public static JPanel contentPane;
-    public static JPanel panelMainContent;
 
     private Image img_logo = new ImageIcon(LoginFrame.class.getResource("/Res/bank.png")).getImage().getScaledInstance(90, 90, Image.SCALE_SMOOTH);
     private Image img_home = new ImageIcon(LoginFrame.class.getResource("/Res/home.png")).getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
@@ -45,9 +43,7 @@ public class MainFrame extends JFrame
     {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100,  1000, 750);
-//        setUndecorated(true);
         setLocationRelativeTo(null);
-        //setLayout(null);
         contentPane = new JPanel();
         contentPane.setBackground(Color.WHITE);
         contentPane.setLayout(new BorderLayout(0, 0));
@@ -84,15 +80,8 @@ public class MainFrame extends JFrame
         panelHome = new JPanel();
         panelHome.addMouseListener(new PanelButtonMouseAdapter.PBMainFrame(panelHome, home));
         panelHome.setBackground(new Color(60, 179, 113));
-//        panelHome.setBounds(0, 140, 249, 40);
-//        panelHome.setLayout(null);
         panelMenu.add(panelHome);
 
-//        JLabel lblIconHome = new JLabel("");
-//        lblIconHome.setHorizontalAlignment(SwingConstants.CENTER);
-//        lblIconHome.setBounds(20, 0, 40, 40);
-//        lblIconHome.setIcon(new ImageIcon(img_home));
-//        panelHome.add(lblIconHome);
 
         JLabel lblHome = new JLabel("HOME");
         lblHome.setIcon(new ImageIcon(img_home));
@@ -106,15 +95,8 @@ public class MainFrame extends JFrame
         panelProfits = new JPanel();
         panelProfits.addMouseListener(new PanelButtonMouseAdapter.PBMainFrame(panelProfits, profits));
         panelProfits.setBackground(new Color(47, 79, 79));
-//        panelProfits.setBounds(0, 180, 249, 40);
-//        panelProfits.setLayout(null);
         panelMenu.add(panelProfits);
 
-//        JLabel lblIconProfits = new JLabel("");
-//        lblIconProfits.setHorizontalAlignment(SwingConstants.CENTER);
-//        lblIconProfits.setBounds(20, 0, 40, 40);
-//        lblIconProfits.setIcon(new ImageIcon(img_profits));
-//        panelProfits.add(lblIconProfits);
 
         JLabel lblProfits = new JLabel("PROFILE");
         lblProfits.setIcon(new ImageIcon(img_profits));
@@ -128,15 +110,8 @@ public class MainFrame extends JFrame
         panelOrders = new JPanel();
         panelOrders.addMouseListener(new PanelButtonMouseAdapter.PBMainFrame(panelOrders, orders));
         panelOrders.setBackground(new Color(47, 79, 79));
-//        panelOrders.setBounds(0, 220, 249, 40);
-//        panelOrders.setLayout(null);
         panelMenu.add(panelOrders);
 
-//        JLabel lblIconOrders = new JLabel("");
-//        lblIconOrders.setHorizontalAlignment(SwingConstants.CENTER);
-//        lblIconOrders.setBounds(20, 0, 40, 40);
-//        lblIconOrders.setIcon(new ImageIcon(img_orders));
-//        panelOrders.add(lblIconOrders);
 
         JLabel lblOrders = new JLabel("ORDERS");
         lblOrders.setHorizontalAlignment(SwingConstants.CENTER);
@@ -150,15 +125,8 @@ public class MainFrame extends JFrame
         panelService = new JPanel();
         panelService.addMouseListener(new PanelButtonMouseAdapter.PBMainFrame(panelService, service));
         panelService.setBackground(new Color(47, 79, 79));
-//        panelService.setBounds(0, 260, 249, 40);
-//        panelService.setLayout(null);
         panelMenu.add(panelService);
 
-//        JLabel lblIconService = new JLabel("");
-//        lblIconService.setHorizontalAlignment(SwingConstants.CENTER);
-//        lblIconService.setBounds(20, 0, 40, 40);
-//        lblIconService.setIcon(new ImageIcon(img_service));
-//        panelService.add(lblIconService);
 
         JLabel lblService = new JLabel("SERVICE");
         lblService.setHorizontalAlignment(SwingConstants.CENTER);
@@ -171,15 +139,9 @@ public class MainFrame extends JFrame
         panelSetting = new JPanel();
         panelSetting.addMouseListener(new PanelButtonMouseAdapter.PBMainFrame(panelSetting, setting));
         panelSetting.setBackground(new Color(47, 79, 79));
-//        panelSetting.setBounds(0, 300, 249, 40);
-//        panelSetting.setLayout(null);
         panelMenu.add(panelSetting);
 
-//        JLabel lblIconSetting = new JLabel("");
-//        lblIconSetting.setHorizontalAlignment(SwingConstants.CENTER);
-//        lblIconSetting.setBounds(20, 0, 40, 40);
-//        lblIconSetting.setIcon(new ImageIcon(img_setting));
-//        panelSetting.add(lblIconSetting);
+
 
         JLabel lblSetting = new JLabel("SETTING");
         lblSetting.setHorizontalAlignment(SwingConstants.CENTER);
@@ -220,15 +182,10 @@ public class MainFrame extends JFrame
             }
         });
         panelSignOut.setBackground(new Color(47, 79, 79));
-//        panelSignOut.setBounds(0, 340, 249, 40);
-//        panelSignOut.setLayout(null);
+
         panelMenu.add(panelSignOut);
 
-//        JLabel lblIconSignOut = new JLabel("");
-//        lblIconSignOut.setHorizontalAlignment(SwingConstants.CENTER);
-//        lblIconSignOut.setBounds(20, 0, 40, 40);
-//        lblIconSignOut.setIcon(new ImageIcon(img_sign_out));
-//        panelSignOut.add(lblIconSignOut);
+
 
         JLabel lblSignOut = new JLabel("SIGN OUT");
         lblSignOut.setHorizontalAlignment(SwingConstants.CENTER);
@@ -238,44 +195,9 @@ public class MainFrame extends JFrame
         lblSignOut.setBounds(76, 11, 163, 18);
         panelSignOut.add(lblSignOut);
 
-//        JLabel lblX = new JLabel("X");
-//        lblX.addMouseListener(new MouseAdapter()
-//        {
-//            @Override
-//            public void mouseClicked(MouseEvent e)
-//            {
-//                if (JOptionPane.showConfirmDialog(null, "Are you sure you want to close this application?", "Confirmation", JOptionPane.YES_NO_OPTION) == 0)
-//                {
-//                    MainFrame.this.dispose();
-//                }
-//            }
-//
-//            @Override
-//            public void mouseEntered(MouseEvent event)
-//            {
-//                lblX.setForeground(Color.RED);
-//            }
-//
-//            @Override
-//            public void mouseExited(MouseEvent event)
-//            {
-//                lblX.setForeground(Color.BLACK);
-//            }
-//        });
-//        lblX.setForeground(Color.BLACK);
-//        lblX.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
-//        lblX.setHorizontalAlignment(SwingConstants.CENTER);
-//        lblX.setBounds(788, -2, 20, 20);
-//        contentPane.add(lblX);
 
+        contentPane.add(home, "Center");
 
-        panelMainContent = new JPanel();
-//        panelMainContent.setBounds(249, 15, 558, 420);
-//        panelMainContent.setLayout(null); // nếu k có layout sẽ năm tứ tung và có thể k xuất hiện
-        panelMainContent.setLayout(new BorderLayout(0,0));
-        contentPane.add(panelMainContent, "Center");
-        // sau khi đăng nhập sẽ mặc định trỏ vào trang home
-        panelMainContent.add(home, "Center");
 
     }
 
@@ -288,7 +210,7 @@ public class MainFrame extends JFrame
         setting.setVisible(false);
 
         panel.setVisible(true);
-        panelMainContent.add(panel, "North");
+        contentPane.add(panel, "Center");
     }
 
     public static void Clicked(JPanel panel)
--- BankManager/src/GUI/PanelTransfer.java
+++ BankManager/src/GUI/PanelTransfer.java
@@ -1,54 +1,77 @@
 package GUI;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+
 
 public class PanelTransfer extends JPanel
 {
-    private FlowLayout flowLayout = new FlowLayout();
 
     public PanelTransfer()
     {
-        flowLayout.setAlignment(FlowLayout.CENTER);
-        flowLayout.setHgap(80);
-        flowLayout.setVgap(40);
-        this.setLayout(flowLayout);
-        this.setBackground(Color.WHITE);
+        JPanel containAccountNumber = new JPanel();
+        containAccountNumber.setLayout(new FlowLayout(FlowLayout.RIGHT, 20, 10));
 
-        JPanel panel = new RoundedPanel(10, Color.RED);
-        panel.add(new JLabel("Ngu")); // do có khởi tạo ở đây nên k setLayout(null)
-        panel.setBounds(10, 30, 100, 60);
-        panel.setOpaque(false);
-        this.add(panel);
+        JPanel accountNumber = new RoundedPanel(20, Color.WHITE);
+        accountNumber.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 8));
+        containAccountNumber.add(accountNumber);
 
-        JLabel lblBack = new JLabel("<");
-        lblBack.addMouseListener(new MouseAdapter()
-        {
-            @Override
-            public void mouseClicked(MouseEvent e)
-            {
-//                PanelService.panelMainContent.setVisible(true);
-                PanelTransfer.this.setVisible(false);
-            }
+        JTextField txtAccountNumber = new JTextField();
+        txtAccountNumber.setBackground(Color.WHITE);
+        txtAccountNumber.setBorder(null);
+        txtAccountNumber.setColumns(25);
+        txtAccountNumber.setFont(new Font("Arial", Font.PLAIN, 12));
+        accountNumber.add(txtAccountNumber);
 
-            @Override
-            public void mouseEntered(MouseEvent event)
-            {
-                lblBack.setForeground(Color.RED);
-            }
+        JPanel containAmount = new JPanel();
+        containAmount.setLayout(new FlowLayout(FlowLayout.RIGHT, 20, 10));
 
-            @Override
-            public void mouseExited(MouseEvent event)
-            {
-                lblBack.setForeground(Color.BLACK);
-            }
-        });
-        lblBack.setForeground(Color.BLACK);
-        lblBack.setFont(new Font("Comic Sans MS", Font.BOLD, 18));
-        lblBack.setHorizontalAlignment(SwingConstants.CENTER);
-        lblBack.setBounds(0, -2, 20, 20);
-        this.add(lblBack);
+        JPanel amount = new RoundedPanel(20, Color.WHITE);
+        amount.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 8));
+        containAmount.add(amount);
+
+        JTextField txtAmount = new JTextField();
+        txtAmount.setBackground(Color.WHITE);
+        txtAmount.setBorder(null);
+        txtAmount.setColumns(25);
+        txtAmount.setFont(new Font("Arial", Font.PLAIN, 12));
+        amount.add(txtAmount);
+
+        JPanel containContent = new JPanel();
+        containContent.setLayout(new FlowLayout(FlowLayout.RIGHT, 20, 10));
+
+        JPanel content = new RoundedPanel(20, Color.WHITE);
+        content.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 8));
+        containContent.add(content);
+
+        JTextArea txtContent = new JTextArea();
+        txtContent.setBackground(Color.WHITE);
+        txtContent.setLineWrap(true);
+        txtContent.setBorder(null);
+        txtContent.setRows(15);
+        txtContent.setColumns(25);
+        txtContent.setFont(new Font("Arial", Font.PLAIN, 12));
+        content.add(txtContent);
+
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.CENTER;
+        this.add(new JLabel("Enter account number need transfer"), gbc);
+        gbc.gridx += 1;
+        this.add(containAccountNumber, gbc);
+        gbc.gridy++;
+        gbc.gridx = 0;
+        this.add(new JLabel("Amount"), gbc);
+        gbc.gridx += 1;
+        this.add(containAmount, gbc);
+        gbc.gridy++;
+        gbc.gridx = 0;
+        this.add(new JLabel("Content"), gbc);
+        gbc.gridx += 1;
+        this.add(containContent, gbc);
+
     }
 }
