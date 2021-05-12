6975770203eaef1d48bea3c277b10fd96bc95bf8
DESKTOP-0L66HQK\ADMINS <vodoanhoanglong10a9@gmail.com>
2021-05-12 20:47:55 +0700

    add page Profile

--- BankManager/src/Views/PanelProfile.java
+++ BankManager/src/Views/PanelProfile.java
@@ -1,7 +1,19 @@
 package Views;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PanelProfile extends JPanel
 {
+    public PanelProfile()
+    {
+        this.setLayout(new BorderLayout(0,0));
+        this.add(new JPanel(), "West");
+        this.add(new JPanel(), "East");
+
+        JPanel panelMain = new RadiusAndShadow();
+        panelMain.setBackground(Color.WHITE);
+        panelMain.setPreferredSize(new Dimension(300,0));
+        this.add(panelMain, "Center");
+    }
 }
