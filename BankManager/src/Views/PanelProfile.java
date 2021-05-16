9bd888310812b74afdfbf568a1f42d5282f9cf15
Hoang Long <vodoanhoanglong10a9@gmail.com>
2021-05-16 16:17:38 +0700

    Add

--- BankManager/src/Views/PanelProfile.java
+++ BankManager/src/Views/PanelProfile.java
@@ -12,7 +12,7 @@ public class PanelProfile extends JPanel
     public static Image img_woman = new ImageIcon(PanelProfile.class.getResource("/Res/avatar_woman.png")).getImage().getScaledInstance(180, 180, Image.SCALE_SMOOTH);
     public static Image img_man = new ImageIcon(PanelProfile.class.getResource("/Res/avatar_man.png")).getImage().getScaledInstance(180, 180, Image.SCALE_SMOOTH);
     public static JLabel lblAvatar;
-    public static JPanel panelAvatar;
+
 
     public PanelProfile()
     {
@@ -33,7 +33,7 @@ public class PanelProfile extends JPanel
             lblAvatar.setIcon(new ImageIcon(img_man));
         else lblAvatar.setIcon(new ImageIcon(img_woman));
 
-        panelAvatar = new JPanel();
+        JPanel panelAvatar = new JPanel();
         panelAvatar.setBackground(Color.WHITE);
         panelAvatar.setLayout(new FlowLayout(FlowLayout.CENTER,0,20));
         panelAvatar.add(lblAvatar);
@@ -48,12 +48,12 @@ public class PanelProfile extends JPanel
         panelProfile.add(lblProfile);
 
         panelHeader.add(panelAvatar);
-        panelHeader.add(Box.createRigidArea(new Dimension(0, 20)));
+        panelHeader.add(Box.createRigidArea(new Dimension(20, 20)));
         panelHeader.add(panelProfile);
 
         JPanel panelMain = new JPanel();
         panelMain.setBackground(Color.WHITE);
-        panelMain.setLayout(new BorderLayout(50,50));
+        panelMain.setLayout(new BorderLayout(100,35));
         panelMain.add(panelHeader, "North");
 
         this.add(panelMain, "Center");
@@ -67,15 +67,14 @@ public class PanelProfile extends JPanel
 
 
         JPanel panelCenter = new JPanel();
-        panelCenter.setLayout(new FlowLayout(FlowLayout.CENTER,20,20));
+        panelCenter.setLayout(new GridLayout(2,0));
         panelCenter.setBackground(Color.WHITE);
         panelMain.add(panelCenter, "Center");
 
-        JPanel panelInformation = new RadiusAndShadow();
-        panelInformation.setPreferredSize(new Dimension(400,200));
-        panelInformation.setBackground(Color.GRAY);
-        panelInformation.setLayout(new GridBagLayout());
-        panelCenter.add(panelInformation);
+        JPanel panelInformation1 = new RadiusAndShadow();
+        panelInformation1.setBackground(Color.GRAY);
+        panelInformation1.setLayout(new GridBagLayout());
+        panelCenter.add(panelInformation1);
 
         JLabel lblFullName = new JLabel("VO DOAN HOANG LONG");
         lblFullName.setForeground(Color.BLACK);
@@ -90,20 +89,20 @@ public class PanelProfile extends JPanel
         gbc.insets = new Insets(10,10,10,10);
         gbc.fill = GridBagConstraints.HORIZONTAL;
 
-        panelInformation.add(lblFullName, gbc);
+        panelInformation1.add(lblFullName, gbc);
 
 
         JLabel lblAccountNumber = new JLabel("21302191932290");
         lblAccountNumber.setForeground(Color.BLACK);
         lblAccountNumber.setFont(new Font("Arial", Font.BOLD, 15));
         gbc.gridy = 1;
-        panelInformation.add(lblAccountNumber, gbc);
+        panelInformation1.add(lblAccountNumber, gbc);
 
         JPanel pnlBalance = new JPanel();
         pnlBalance.setBackground(Color.WHITE);
         pnlBalance.setLayout(new BoxLayout(pnlBalance, BoxLayout.X_AXIS));
         gbc.gridy = 2;
-        panelInformation.add(pnlBalance, gbc);
+        panelInformation1.add(pnlBalance, gbc);
 
         JLabel lblBalance = new JLabel("100.000.000.000.000");
         lblBalance.setForeground(Color.black);
@@ -119,7 +118,7 @@ public class PanelProfile extends JPanel
         lblDate.setForeground(Color.black);
         lblDate.setFont(new Font("Arial", Font.BOLD, 15));
         gbc.gridy = 3;
-        panelInformation.add(lblDate, gbc);
+        panelInformation1.add(lblDate, gbc);
 
         JPanel panelFooter = new JPanel();
         panelFooter.setLayout(new GridBagLayout());
@@ -134,6 +133,10 @@ public class PanelProfile extends JPanel
         gridBagConstraints.weighty = 1;
         gridBagConstraints.weightx = 1;
         gridBagConstraints.fill = GridBagConstraints.BOTH;
+
+        JPanel panelInformation2 = new RadiusAndShadow();
+        panelInformation2.setLayout(new BoxLayout(panelInformation2, BoxLayout.Y_AXIS));
+        panelCenter.add(panelInformation2);
 
         JPanel panelEditInf = new RadiusAndShadow();
         panelEditInf.setBackground(Color.GRAY);
