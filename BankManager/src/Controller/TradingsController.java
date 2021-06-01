ad76a0f4cd3deeca4d57cda2874499059f45830b
DESKTOP-0L66HQK\ADMINS <vodoanhoanglong10a9@gmail.com>
2021-06-01 18:31:36 +0700

    Finished fix filter

--- BankManager/src/Controller/TradingsController.java
+++ BankManager/src/Controller/TradingsController.java
@@ -13,6 +13,7 @@ public class TradingsController
     public static double totalSpendingValue;
     public static double totalReceivedValue;
     public static int flag = 0;
+
     public static void uploadAllTradingData(JTable table, String accountNumber)
     {
         ResultSet resultSet = TradingsData.getAllTrading(accountNumber);
@@ -23,39 +24,41 @@ public class TradingsController
     {
         ResultSet resultSet;
         flag = 0;
-        if(type.equals("All"))
+        if (type.equals("All"))
             type = "Tất Cả";
-        else if(type.equals("Recharge"))
+        else if (type.equals("Recharge"))
             type = "Nạp tiền";
-        else if(type.equals("Withdraw"))
+        else if (type.equals("Withdraw"))
             type = "Rút tiền";
-        else if(type.equals("Transfer"))
+        else if (type.equals("Transfer"))
             type = "Chuyển khoản";
         else type = "Nhận chuyển khoản";
+
         if (type.equals("Tất Cả"))
         {
             uploadAllTradingData(table, accountNumber);
             return;
-        }else if(type.equals("Chuyển khoản"))
+        } else if (type.equals("Chuyển khoản"))
         {
             resultSet = TradingsData.getTradedByType("Chuyển tiền", accountNumber);
             flag = 1;
-            showHistoryTrading(table, resultSet);
+            showHistoryTrading3(table, resultSet);
             return;
-        }else if(type.equals("Nhận chuyển khoản"))
+        } else if (type.equals("Nhận chuyển khoản"))
         {
             resultSet = TradingsData.getTradedByType("Chuyển tiền", accountNumber);
             flag = 2;
-            showHistoryTrading(table, resultSet);
+            showHistoryTrading3(table, resultSet);
             return;
         }
         resultSet = TradingsData.getTradedByType(type, accountNumber);
-        showHistoryTrading(table, resultSet);
+        showHistoryTrading2(table, resultSet);
     }
 
 
     public static void showHistoryTrading(JTable table, ResultSet resultSet)
     {
+
         totalSpendingValue = 0;
         totalReceivedValue = 0;
         DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
@@ -67,46 +70,26 @@ public class TradingsController
             {
                 if (resultSet.getString("LoaiGD").equals("Chuyển tiền"))
                 {
-                    if(flag==0)
+                    if (LoginController.accountNumber.equals(resultSet.getString("SoTK")))
                     {
-                        if (LoginController.accountNumber.equals(resultSet.getString("SoTK")))
-                        {
-                            totalSpendingValue += resultSet.getDouble("SoTien");
-                            dataObjects[0] = "Chuyển khoản";
-                            dataObjects[1] = resultSet.getString("NgayGD");
-                            dataObjects[2] = getUserName(resultSet.getString("SoTKNhan"));
-                            dataObjects[3] = resultSet.getString("GhiChu");
-                            dataObjects[4] = String.format("%,.0f", resultSet.getDouble("SoTien"));
-                        } else if (LoginController.accountNumber.equals(resultSet.getString("SoTKNhan")))
-                        {
-                            totalReceivedValue += resultSet.getDouble("SoTien");
-                            dataObjects[0] = "Nhận chuyển khoản";
-                            dataObjects[1] = resultSet.getString("NgayGD");
-                            dataObjects[2] = getUserName(resultSet.getString("SoTK"));
-                            dataObjects[3] = resultSet.getString("GhiChu");
-                            dataObjects[4] = String.format("%,.0f", resultSet.getDouble("SoTien"));
-                        }
-                    }else
+                        totalSpendingValue += resultSet.getDouble("SoTien");
+                        dataObjects[0] = "Chuyển khoản";
+                        dataObjects[1] = resultSet.getString("NgayGD");
+                        dataObjects[2] = getUserName(resultSet.getString("SoTKNhan"));
+                        dataObjects[3] = resultSet.getString("GhiChu");
+                        dataObjects[4] = String.format("%,.0f", resultSet.getDouble("SoTien"));
+                    } else if (LoginController.accountNumber.equals(resultSet.getString("SoTKNhan")))
                     {
-                        if(flag==1)
-                        {
-                            dataObjects[0] = "Chuyển khoản";
-                            dataObjects[1] = resultSet.getString("NgayGD");
-                            dataObjects[2] = getUserName(resultSet.getString("SoTKNhan"));
-                            dataObjects[3] = resultSet.getString("GhiChu");
-                            dataObjects[4] = String.format("%,.0f", resultSet.getDouble("SoTien"));
-                        }else
-                        {
-                            dataObjects[0] = "Nhận chuyển khoản";
-                            dataObjects[1] = resultSet.getString("NgayGD");
-                            dataObjects[2] = getUserName(resultSet.getString("SoTK"));
-                            dataObjects[3] = resultSet.getString("GhiChu");
-                            dataObjects[4] = String.format("%,.0f", resultSet.getDouble("SoTien"));
-                        }
+                        totalReceivedValue += resultSet.getDouble("SoTien");
+                        dataObjects[0] = "Nhận chuyển khoản";
+                        dataObjects[1] = resultSet.getString("NgayGD");
+                        dataObjects[2] = getUserName(resultSet.getString("SoTK"));
+                        dataObjects[3] = resultSet.getString("GhiChu");
+                        dataObjects[4] = String.format("%,.0f", resultSet.getDouble("SoTien"));
                     }
                 } else
                 {
-                    if(resultSet.getString("LoaiGD").equals("Rút tiền"))
+                    if (resultSet.getString("LoaiGD").equals("Rút tiền"))
                         totalSpendingValue += resultSet.getDouble("SoTien");
                     else totalReceivedValue += resultSet.getDouble("SoTien");
                     dataObjects[0] = resultSet.getString("LoaiGD");
@@ -114,6 +97,67 @@ public class TradingsController
                     dataObjects[2] = getUserName(resultSet.getString("SoTK"));
                     dataObjects[3] = resultSet.getString("GhiChu");
                     dataObjects[4] = String.format("%,.0f", resultSet.getDouble("SoTien"));
+                }
+                tableModel.addRow(dataObjects);
+            }
+        } catch (SQLException e)
+        {
+            System.out.println("Load Data Fail");
+        }
+    }
+
+    public static void showHistoryTrading2(JTable table, ResultSet resultSet)
+    {
+
+        DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
+        tableModel.setRowCount(0);
+        Object[] dataObjects = new Object[5];
+        try
+        {
+            while (resultSet.next())
+            {
+                dataObjects[0] = resultSet.getString("LoaiGD");
+                dataObjects[1] = resultSet.getString("NgayGD");
+                dataObjects[2] = getUserName(resultSet.getString("SoTK"));
+                dataObjects[3] = resultSet.getString("GhiChu");
+                dataObjects[4] = String.format("%,.0f", resultSet.getDouble("SoTien"));
+                tableModel.addRow(dataObjects);
+            }
+        } catch (SQLException e)
+        {
+            System.out.println("Load Data Fail");
+        }
+    }
+
+    public static void showHistoryTrading3(JTable table, ResultSet resultSet)
+    {
+        DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
+        tableModel.setRowCount(0);
+        Object[] dataObjects = new Object[5];
+        try
+        {
+            while (resultSet.next())
+            {
+                if (flag == 1)
+                {
+                    if (LoginController.accountNumber.equals(resultSet.getString("SoTK")))
+                    {
+                        dataObjects[0] = "Chuyển khoản";
+                        dataObjects[1] = resultSet.getString("NgayGD");
+                        dataObjects[2] = getUserName(resultSet.getString("SoTKNhan"));
+                        dataObjects[3] = resultSet.getString("GhiChu");
+                        dataObjects[4] = String.format("%,.0f", resultSet.getDouble("SoTien"));
+                    }else continue;
+                } else
+                {
+                    if (LoginController.accountNumber.equals(resultSet.getString("SoTKNhan")))
+                    {
+                        dataObjects[0] = "Nhận chuyển khoản";
+                        dataObjects[1] = resultSet.getString("NgayGD");
+                        dataObjects[2] = getUserName(resultSet.getString("SoTK"));
+                        dataObjects[3] = resultSet.getString("GhiChu");
+                        dataObjects[4] = String.format("%,.0f", resultSet.getDouble("SoTien"));
+                    }else continue;
                 }
                 tableModel.addRow(dataObjects);
             }
@@ -137,13 +181,17 @@ public class TradingsController
         }
         return result;
     }
-    public static void uploadTradingDataOverview(JTable table, String accountNumber){
+
+    public static void uploadTradingDataOverview(JTable table, String accountNumber)
+    {
         DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
         tableModel.setRowCount(0);
         Object[] dataObjects = new Object[3];
         ResultSet resultSet = TradingsData.getTop10Trading(accountNumber);
-        try{
-            while (resultSet.next()){
+        try
+        {
+            while (resultSet.next())
+            {
                 if (resultSet.getString("LoaiGD").equals("Chuyển tiền"))
                 {
                     if (LoginController.accountNumber.equals(resultSet.getString("SoTK")))
@@ -165,7 +213,8 @@ public class TradingsController
                 }
                 tableModel.addRow(dataObjects);
             }
-        }catch (SQLException e){
+        } catch (SQLException e)
+        {
             System.err.println(e.getMessage());
         }
     }
@@ -183,21 +232,20 @@ public class TradingsController
                 {
                     if (LoginController.accountNumber.equals(resultSet.getString("SoTK")))
                     {
-                        if(date.equals(new SimpleDateFormat("dd/MM/yyyy").format(resultSet.getDate("NgayGD"))))
+                        if (date.equals(new SimpleDateFormat("dd/MM/yyyy").format(resultSet.getDate("NgayGD"))))
                         {
                             spendingDay += resultSet.getDouble("SoTien");
-                        }
-                        else spendingDay += 0;
+                        } else spendingDay += 0;
                     }
-                }else
+                } else
                 {
-                    if(resultSet.getString("LoaiGD").equals("Rút tiền"))
-                        if(date.equals(new SimpleDateFormat("dd/MM/yyyy").format(resultSet.getDate("NgayGD"))))
+                    if (resultSet.getString("LoaiGD").equals("Rút tiền"))
+                        if (date.equals(new SimpleDateFormat("dd/MM/yyyy").format(resultSet.getDate("NgayGD"))))
                             result += resultSet.getDouble("SoTien");
                         else result += 0;
                 }
             }
-        }catch (Exception e)
+        } catch (Exception e)
         {
             System.err.println("TradingsCotroller.getUsersSpendingPerDay: " + e.getMessage());
         }
@@ -217,21 +265,20 @@ public class TradingsController
                 {
                     if (LoginController.accountNumber.equals(resultSet.getString("SoTKNhan")))
                     {
-                        if(date.equals(new SimpleDateFormat("dd/MM/yyyy").format(resultSet.getDate("NgayGD"))))
+                        if (date.equals(new SimpleDateFormat("dd/MM/yyyy").format(resultSet.getDate("NgayGD"))))
                         {
                             receivedDay += resultSet.getDouble("SoTien");
-                        }
-                        else receivedDay += 0;
+                        } else receivedDay += 0;
                     }
-                }else
+                } else
                 {
-                    if(resultSet.getString("LoaiGD").equals("Nạp tiền"))
-                        if(date.equals(new SimpleDateFormat("dd/MM/yyyy").format(resultSet.getDate("NgayGD"))))
+                    if (resultSet.getString("LoaiGD").equals("Nạp tiền"))
+                        if (date.equals(new SimpleDateFormat("dd/MM/yyyy").format(resultSet.getDate("NgayGD"))))
                             result += resultSet.getDouble("SoTien");
                         else result += 0;
                 }
             }
-        }catch (Exception e)
+        } catch (Exception e)
         {
             System.err.println("TradingsCotroller.getUsersSpendingPerDay: " + e.getMessage());
         }
--- BankManager/src/Views/PanelHome.java
+++ BankManager/src/Views/PanelHome.java
@@ -5,19 +5,24 @@ import java.awt.*;
 
 public class PanelHome extends JPanel
 {
-    private Image avatar = new ImageIcon(PanelHome.class.getResource("/Res/avartar.png")).getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+
+    private Image a1 = new ImageIcon(LoginFrame.class.getResource("/Res/avatar1.png")).getImage().getScaledInstance(200, 100, Image.SCALE_SMOOTH);
+    private Image a2 = new ImageIcon(LoginFrame.class.getResource("/Res/avatar2.png")).getImage().getScaledInstance(200, 100, Image.SCALE_SMOOTH);
+    private Image a3 = new ImageIcon(LoginFrame.class.getResource("/Res/avatar3.png")).getImage().getScaledInstance(200, 100, Image.SCALE_SMOOTH);
+    private Image a4 = new ImageIcon(LoginFrame.class.getResource("/Res/avatar4.png")).getImage().getScaledInstance(200, 100, Image.SCALE_SMOOTH);
+
     public PanelHome()
     {
-        this.setLayout(new BorderLayout(0,0));
+        this.setLayout(new BorderLayout(0, 0));
 
         JPanel panelHeader = new LinearGradient(1);
         panelHeader.setBackground(Color.WHITE);
         panelHeader.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 20));
 
         JPanel panelTitle = new RadiusAndShadow();
-        panelTitle.setPreferredSize(new Dimension(200,70));
+        panelTitle.setPreferredSize(new Dimension(200, 70));
         panelTitle.setBackground(Color.WHITE);
-        panelTitle.setLayout(new FlowLayout(FlowLayout.CENTER,0,13));
+        panelTitle.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 13));
         panelHeader.add(panelTitle);
 
         JLabel lblTitle = new JLabel();
@@ -29,24 +34,39 @@ public class PanelHome extends JPanel
 
         this.add(panelHeader, "North");
 
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.WHITE);
+        panel.setLayout(new BorderLayout(50, 0));
+        panel.add(new JPanel(), "West");
+        panel.add(new JPanel(), "East");
 
+        JLabel l1 = new JLabel("");
+        l1.setIcon(new ImageIcon(a1));
+        JLabel l2 = new JLabel("");
+        l2.setIcon(new ImageIcon(a2));
+        JLabel l3 = new JLabel("");
+        l3.setIcon(new ImageIcon(a3));
+        JLabel l4 = new JLabel("");
+        l4.setIcon(new ImageIcon(a4));
 
-        JLabel lblIcon = new JLabel("");
-        lblIcon.setIcon(new ImageIcon(avatar));
-
-        JPanel panelFooter = new LinearGradient(1);
+        JPanel panelFooter = new JPanel();
         panelFooter.setBackground(Color.WHITE);
-        panelFooter.setPreferredSize(new Dimension(0,120));
-        panelFooter.setLayout(new FlowLayout());
-        panelFooter.add(lblIcon);
-        this.add(panelFooter, "South");
+        panelFooter.setLayout(new FlowLayout(FlowLayout.CENTER, 50,0));
+
+        panelFooter.add(l1);
+        panelFooter.add(l2);
+        panelFooter.add(l3);
+        panelFooter.add(l4);
+        panelFooter.setPreferredSize(new Dimension(0, 100));
+        panel.add(panelFooter, "Center");
+        this.add(panel, "South");
 
 
         JPanel panelCenterMain = new JPanel();
         panelCenterMain.setBackground(Color.WHITE);
-        panelCenterMain.setLayout(new BorderLayout(10,0));
+        panelCenterMain.setLayout(new BorderLayout(10, 0));
         panelCenterMain.add(new ScaleImg("/Res/img_home2.png"), "Center");
-        panelCenterMain.setPreferredSize(new Dimension(150,0));
+        panelCenterMain.setPreferredSize(new Dimension(150, 0));
         this.add(panelCenterMain, "Center");
 
         JPanel panelRight = new JPanel();
@@ -56,12 +76,12 @@ public class PanelHome extends JPanel
 
         JPanel panelNorth = new JPanel();
         panelNorth.setBackground(Color.WHITE);
-        panelNorth.setPreferredSize(new Dimension(100,20));
+        panelNorth.setPreferredSize(new Dimension(100, 20));
         panelCenterMain.add(panelNorth, "North");
 
         JPanel panelWest = new JPanel();
         panelWest.setBackground(Color.WHITE);
-        panelWest.setPreferredSize(new Dimension(0,100));
+        panelWest.setPreferredSize(new Dimension(0, 100));
         panelCenterMain.add(panelWest, "West");
 
 
@@ -69,7 +89,7 @@ public class PanelHome extends JPanel
         panelEast.setBackground(Color.WHITE);
         panelEast.setLayout(new BoxLayout(panelEast, BoxLayout.Y_AXIS));
         panelEast.add(new ScaleImg("/Res/img_home1.png"));
-        panelEast.add(Box.createRigidArea(new Dimension(0,10)));
+        panelEast.add(Box.createRigidArea(new Dimension(0, 10)));
         panelEast.add(new ScaleImg("/Res/img_home3.png"));
         panelCenterMain.add(panelEast, "East");
     }
